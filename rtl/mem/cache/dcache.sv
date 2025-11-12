1230bd80d03dac9cc5cfaf7719e0a9e0f5f58c98
Trevor Cannon <trevpcannon@gmail.com>
2025-11-12 15:16:33 -0500

    feat: semi skeleton code beginning for instruction cache, added L2 cache file

--- rtl/mem/cache/dcache.sv
+++ rtl/mem/cache/dcache.sv
@@ -3,7 +3,7 @@ module dcache #(
     parameter ADDR_WIDTH = 32,
     parameter LINE_SIZE = 64,
     parameter WAYS = 4,
-    parameter DMEM_SIZE = 32768
+    parameter DMEM_SIZE = 16384
 ) (
     ports
 );
