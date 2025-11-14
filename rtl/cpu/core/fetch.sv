da4225086ba0cab0dc5f467bf32331fb8a804ad9
Arman Imam <armanimam80@gmail.com>
2025-11-14 17:20:35 -0500

    Changed from verilog to systemverilog
    Was unaware that the code should be in systemverilog.

--- rtl/cpu/core/fetch.sv
+++ rtl/cpu/core/fetch.sv
@@ -1,22 +1,22 @@
 //Fetch Module
-
+'include "rv32_pgk.cv"
 module fetch(
-    input [31:0] pc_q,          //Program Counter, Tells I-Cache where to look
-    input redir_i,              //Tells Fetch to redirect the PC
-
+    input logic [31:0] pc_q,          //Program Counter, Tells I-Cache where to look
+    input logic redir_i,              //Tells Fetch to redirect the PC
+    input logic clk, rst              //Clock, Reset 
     //I-cache interface: stores instructions for quick access
     //req: asks for instruction
-    output ic_req_valid,        //Fetch requests instruction from I-cache
-    output [31:0] ic_req_addr,  //Address of the instruction being requested
+    input logic ic_req_valid,        //Fetch requests instruction from I-cache
+    input logic [31:0] ic_req_addr,  //Address of the instruction being requested
 
     //rsp: recieves the instruction
-    input ic_rsp_valid,         //Fetch receives the instruction
-    input [31:0] ic_rsp_data,   //Fetch takes the data from instruction and uses it
+    output logic ic_rsp_valid,         //Fetch receives the instruction
+    output logic [31:0] ic_rsp_data,   //Fetch takes the data from instruction and uses it
 
     //Outputs for Decode
     //IN PROGRESS
-    output [31:0] instr_i,      //Contents of instruction
-    output [31:0] rf_rdata1,    //Address of the instruction
+    output logic [31:0] instr_i,      //Contents of instruction
+    output logic [31:0] rf_rdata1,    //Address of the instruction
 );
 
 endmodule
