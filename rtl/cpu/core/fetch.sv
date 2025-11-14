b0f834f8b3d8a39912d4097d26881e89ecd7e9e1
Arman Imam <armanimam80@gmail.com>
2025-11-14 17:59:06 -0500

    Minor spelling mistake for rv32_pkg name

--- rtl/cpu/core/fetch.sv
+++ rtl/cpu/core/fetch.sv
@@ -1,13 +1,13 @@
 //Fetch Module
-'include "rv32_pgk.cv"
+'include "rv32_pkg.cv"
 module fetch(
     input logic [31:0] pc_q,          //Program Counter, Tells I-Cache where to look
     input logic redir_i,              //Tells Fetch to redirect the PC
     input logic clk, rst              //Clock, Reset 
     //I-cache interface: stores instructions for quick access
     //req: asks for instruction
-    input logic ic_req_valid,        //Fetch requests instruction from I-cache
-    input logic [31:0] ic_req_addr,  //Address of the instruction being requested
+    input logic ic_req_valid,         //Fetch requests instruction from I-cache
+    input logic [31:0] ic_req_addr,   //Address of the instruction being requested
 
     //rsp: recieves the instruction
     output logic ic_rsp_valid,         //Fetch receives the instruction
@@ -16,7 +16,10 @@ module fetch(
     //Outputs for Decode
     //IN PROGRESS
     output logic [31:0] instr_i,      //Contents of instruction
-    output logic [31:0] rf_rdata1,    //Address of the instruction
+    output logic [31:0] rf_rdata1     //Address of the instruction
 );
+//Logic IN PROGRESS /**
+
+
 
 endmodule
