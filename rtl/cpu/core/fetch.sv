a1af5104cbde52400aa9c854990536d670f7e1aa
Arman Imam <armanimam80@gmail.com>
2025-11-14 14:21:18 -0500

    Updated fetch module
    I've updated the fetch module, though more updates may be needed. I still need to update the output for decode.

--- rtl/cpu/core/fetch.sv
+++ rtl/cpu/core/fetch.sv
@@ -1,1 +1,22 @@
-// Test
+//Fetch Module
+
+module fetch(
+    input [31:0] pc_q,          //Program Counter, Tells I-Cache where to look
+    input redir_i,              //Tells Fetch to redirect the PC
+
+    //I-cache interface: stores instructions for quick access
+    //req: asks for instruction
+    output ic_req_valid,        //Fetch requests instruction from I-cache
+    output [31:0] ic_req_addr,  //Address of the instruction being requested
+
+    //rsp: recieves the instruction
+    input ic_rsp_valid,         //Fetch receives the instruction
+    input [31:0] ic_rsp_data,   //Fetch takes the data from instruction and uses it
+
+    //Outputs for Decode
+    //IN PROGRESS
+    output [31:0] instr_i,      //Contents of instruction
+    output [31:0] rf_rdata1,    //Address of the instruction
+);
+
+endmodule
