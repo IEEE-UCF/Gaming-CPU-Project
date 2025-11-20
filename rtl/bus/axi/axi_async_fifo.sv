57aa62fd89d6e3e2a95a8eabe2175de4aca53e6d
XyzalAxel <145172488+XyzalAxel@users.noreply.github.com>
2025-11-20 18:46:32 -0500

    feat: updated axi_async_fifo.sv per request

--- rtl/bus/axi/axi_async_fifo.sv
+++ rtl/bus/axi/axi_async_fifo.sv
@@ -1,16 +1,16 @@
 module axi_async_fifo #(
     // TODO: add parameters, subject to change (widths, depth)
-    parameter int unsigned ADDR_WIDTH = 32;
-    parameter int unsigned DATA_WIDTH = 64;
-    parameter int unsigned ID_WIDTH = 4;
-    parameter int unsigned FIFO_DEPTH = 8;
+    parameter int unsigned ADDR_WIDTH = 32,
+    parameter int unsigned DATA_WIDTH = 64,
+    parameter int unsigned ID_WIDTH = 4,
+    parameter int unsigned FIFO_DEPTH = 8,
 )(
     // Source clock domain
     input logic s_clk_i,
     input logic s_rst_ni,
     
     // Destination clock domain
-    input logic m_clck_i,
+    input logic m_clk_i,
     input logic m_rst_ni
     
     // TODO: add AXI ports for source and destination
@@ -23,3 +23,4 @@ module axi_async_fifo #(
 
 endmodule    
     
+
