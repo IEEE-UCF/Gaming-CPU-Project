1ed805d017dccea8f6d08b12a106f8b138c57e23
Beyondthere <mattkrum14@gmail.com>
2025-11-21 08:14:13 -0500

    Update uart16550.sv

--- rtl/io/uart16550.sv
+++ rtl/io/uart16550.sv
@@ -1,2 +1,5 @@
 /***************************RECEIVE rx_i*******************************/
 // Serial input -> shift register -> FIFO
+
+/***************************FIFO MANAGEMENT****************************/
+// Push/pop data, update pointers and count
