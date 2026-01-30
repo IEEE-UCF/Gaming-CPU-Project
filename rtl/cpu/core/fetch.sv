8c56bd82a3f8dc0613436791305c476bd3663a46
Arman Imam <armanimam80@gmail.com>
2026-01-29 20:15:46 -0500

    Update fetch.sv
    I've turned the always combinational into sequential. I've noted that the pc logic will be handled by the core file, and is now commented out.  For the Cache Response Logic, I've set Inst_o to 32'b0 (invalid signal) for now, until I can verify with the decode stage on errors.

--- rtl/cpu/core/fetch.sv
+++ rtl/cpu/core/fetch.sv
@@ -1,4 +1,5 @@
 //Fetch Module
+
 module fetch(
 //Global Signals
         input  logic         clk_i, rst_ni,    //Clock, Reset(Active-Low)
@@ -17,37 +18,39 @@ module fetch(
         input  logic [31:0]  ic_rsp_data_i,    //The actual 32-bit instruction returned by I-cache
 
     //Outputs to Decode
-        output logic [31:0] inst_o             //Sends instruction to the Decode Stage
+    reg output logic [31:0]  inst_o             //Sends instruction to the Decode Stage
 );
-    //PC logic
-        //reset    ->PC goes to reset address
-        //redirect ->PC redirects to new address ???
-        //PC increments by 4 by default
-    always_ff @(posedge clk_i)begin 
-        if(!rst_ni) pc_q <= 32'b0;
-
-        else if(redir_i) pc_q <= //??
-
-        else pc_q <= pc_q + 4;
-    end
 
     //Cache Request Logic: Instruction is requested by default, unless reset or redirect occurs
-    always_comb() begin
-        if(!rst_ni || redir_i)begin //Doesn't request an instruction
-            ic_req_valid_o = 0;
-            ic_req_addr_o = pc_q;
+    always_ff(posedge clk_i) begin
+        if(!rst_ni || redir_i)begin  //Doesn't request an instruction
+            ic_req_valid_o <= 0;
+            ic_req_addr_o  <= pc_q;
         end
         else begin
-            ic_req_valid_o = 1;     //Instruction is requested
-            ic_req_addr_o = pc_q;
+            ic_req_valid_o <= 1;     //Instruction is requested
+            ic_req_addr_o  <= pc_q;
         end
     end
     //Cache Response Logic: if cache responds, then instruction sent to decode
-    //Otherwise, ???
-    always_comb()begin
-         if(ic_rsp_valid_i) inst_o = ic_rsp_data_i;
-         else inst_o =  //????
+    //Otherwise, send decode invalid data (32'b0)
+    always_ff(posedge clk_i)begin
+         if(redir_i)               inst_o <= 32'b0; //ASK DECODE STAGE
+         else if (!ic_rsp_valid_i) inst_o <= 32'b0; //ASK DECODE STAGE
+         else                      inst_o <= ic_rsp_data_i;
+
+        //flush old instructions for redir/reset
+        //REDIRECT SHOULD STALL
     end
 
-endmodule
 
+/*CORE DEALS WITH THIS
+    //PC logic
+    always_ff @(posedge clk_i)begin 
+        if(!rst_ni) pc_q <= 32'b0;                      //reset    ->PC goes to reset address
+        else if(redir_i) pc_q <= Branch/jump address;   //redirect ->PC redirects to Branch/jump address
+        else pc_q <= pc_q + 4;                          //PC increments by 4 by default
+    end
+*/
+
+endmodule
