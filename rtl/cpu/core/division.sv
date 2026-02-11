45de86884c298bb5a76e9c14e34280229f28bb7c
Adrian <adrianeme134@gmail.com>
2026-02-10 22:55:10 -0500

    Update to division & execute
    cleaned up division module, added better comments to describe intent, still needs signed division and zero edge case

--- rtl/cpu/core/division.sv
+++ rtl/cpu/core/division.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 // Restoring division, divisor initially left-shifted by 32, quotient built MSB→LSB, divisor shifted right each iteration."
-// refactor and clarify intent, only working version is 7/2 unsigned
+
 
 module division
 import rv32_pkg::*;
@@ -11,8 +11,9 @@ import rv32_pkg::*;
     input logic Division_START, // Start Division
     input logic [31:0] dividend, // numerator
     input logic [31:0] divisor, // denominator
-    output logic [63:0] remainder, // result
-    output logic Division_DONE // done flag
+    output logic [63:0] remainder, // Mod 
+    output logic Division_DONE, // done flag
+    output logic [31:0] quotient // result
     );
     
     logic [31:0] dividend_reg; // internal registers driven by inputs
@@ -24,30 +25,24 @@ import rv32_pkg::*;
 
     
     
-    enum {IDLE,STALL,DONE} current_state, next_state; // Division FSM states 0 or 1
+    enum {IDLE,STALL} current_state, next_state; // Division FSM states 0 or 1
  
  
  always_ff @(posedge clk) begin // Division Stall FSM
-    if(rst==1'b0) begin
+    if(rst==1'b0) begin // Active low reset
         current_state <= IDLE;
     end else begin
         current_state <= next_state;  
         end
-         
-        $display("Current State is: %0s",current_state,);
-        $display("next State is: %0s",next_state,);
-        $display("Division_START : %0b",Division_START,);
-        $display("remainder : %0d",remainder_reg,);
-        $display("divisor : %0d",divisor_reg,);       
+              
  end
 
  always_comb begin // Combinational part of state FSM
    next_state = current_state;
     
     unique case(current_state)
-        IDLE: next_state = (Division_START) ? STALL : IDLE; // When Division_start gets asserted by mux, go to STALL
-        STALL: next_state = (Division_DONE_reg) ? DONE : STALL; // When Division circuit is done on last clk edge, it will assert Division_DONE 1 to indicate DONE STATE
-        DONE: next_state = IDLE;
+        IDLE:next_state = (Division_START) ? STALL : IDLE; // When Division_start gets asserted by mux, go to STALL
+        STALL: next_state = (Division_DONE_reg) ? IDLE : STALL; // When Division circuit is done on last clk edge, it will assert Division_DONE 1 to indicate DONE STATE
         default: next_state = current_state;
     endcase
  
@@ -58,34 +53,40 @@ import rv32_pkg::*;
  
    enum {GREATER_EQUAL_THAN_ZERO,LESS_THAN_ZERO} Test_remainder_flag; // Flags used for test comparison case
   
-   logic signed [63:0] remainder_test_comparison; // Test combinational logic that will be used for testing.
+   // Test combinational logic interface  that will be used for testing.
+   logic signed [63:0] remainder_test_comparison; 
    logic [63:0] remainder_test_mux;
    logic [63:0] divisor_comb;
-   logic [31:0] quotient_comb; // Comnbinational logic that will b driven by always_comb case and then latched on in sequential always_ff
+   logic [31:0] quotient_comb; 
    
-   assign  remainder_test_comparison = remainder_reg - divisor_reg; // Combinational Test remainder case
+   assign  remainder_test_comparison = remainder_reg - divisor_reg; // will subtract remainder from divisor
    
-   assign Test_remainder_flag = (remainder_test_comparison[63]) ? LESS_THAN_ZERO : GREATER_EQUAL_THAN_ZERO; // Will assign a flag if Remainder >= 0 or remainder <0
+   assign Test_remainder_flag = (remainder_test_comparison[63]) ? LESS_THAN_ZERO : GREATER_EQUAL_THAN_ZERO; // will assign a flag based on the remainder
    
    
-    always_comb begin // This always combinational block will assign the sequential remainder register that is clocked
-
+    always_comb begin
+     // Restoring division step:
+     // Try subtracting divisor from remainder.
+     // If result >= 0  keep subtraction and append 1 to quotient.
+     // If result < 0  restore old remainder and append 0 to quotient.
+     // Then shift divisor for next bit position.
+ 
     
-        case(Test_remainder_flag) // Comparison case case to see if test remainder is less than zero or equal to zero
+        case(Test_remainder_flag) // Comparison case case 
             GREATER_EQUAL_THAN_ZERO: begin
-                remainder_test_mux = remainder_test_comparison; // If Remainder >= zero, assign remainder_reg(clocked ff) to be Test_remainder_reg
-                quotient_comb = (quotient_reg << 1) | 1'b1; // shift quotient_reg left and set LSB to 1'b1
-                divisor_comb = divisor_reg >> 1;
+                remainder_test_mux = remainder_test_comparison; // Accept trial remainder (subtract succeeded)
+                quotient_comb = (quotient_reg << 1) | 1'b1; // shift left, add quotient bit = 1
+                divisor_comb = divisor_reg >> 1; // next alignment (shift divisor) we are basically doing long division and comparing bit positions to see if they match
             end
             LESS_THAN_ZERO: begin
-                quotient_comb = (quotient_reg << 1) | 1'b0; // if Remainder <0, shift quotient register left and set LSB to 1'b0
-                remainder_test_mux = remainder_reg;
-                divisor_comb = divisor_reg >> 1;
+                quotient_comb = (quotient_reg << 1) | 1'b0; // Restore remainder (subtract failed)
+                remainder_test_mux = remainder_reg; // Shift left, add quotient bit = 0
+                divisor_comb = divisor_reg >> 1; // Next alignment (shift divisor)
             end
-            default: begin
+            default: begin // default should never happen but keeps tools happy
                 quotient_comb = (quotient_reg << 1) | 1'b0;
-                remainder_test_mux = remainder_reg;
-                divisor_comb = divisor_reg >> 1;
+                remainder_test_mux = remainder_reg; // Restore remainder (subtract failed)
+                divisor_comb = divisor_reg >> 1;// next alignment (shift divisor) we are basically doing long division and comparing bit positions to see if they match
             
             end
         endcase
@@ -101,44 +102,66 @@ import rv32_pkg::*;
             quotient_reg <=  32'd0;
             Division_DONE_reg <= 1'b0; 
             counter <= 6'd0;
+
                  
         end else begin
             case(current_state)
-                IDLE: begin
+                IDLE: begin                               
+                
                     dividend_reg <= dividend; // dividend_reg will latch on to dividend input
-                    divisor_reg <= divisor << 32;   // Divisor_reg will latch on to divisor input
-                    remainder_reg <= {32'd0,dividend};  // remainder_reg will latch on to remainder input            
+                    divisor_reg <= divisor << 32;   // Divisor is aligned to the top 32 bits, divisor_reg is 64 Bit reg
+                    remainder_reg <= {32'd0,dividend};  // remainder_reg will be initialized with numerator            
                     Division_DONE_reg <= 1'b0;
                     counter <= 6'd0;
-                    quotient_reg <=  32'd0;                
-                
+                    quotient_reg <=  32'd0; 
+                                              
                 end
                 STALL: begin
                     remainder_reg <= remainder_test_mux; 
                     quotient_reg <= quotient_comb;                                  
                     divisor_reg <= divisor_comb;
                     counter <= counter + 1'b1;
-                    $display("result : %0",remainder_reg);
-                    $display("quotient : %0d",quotient_reg);
-                    $display("counter : %0d",counter);
-                    $display("denominator : %0d",divisor_reg);
                     
-                    if(counter == 6'd32)
+                    if(counter == 6'd32) begin
                         Division_DONE_reg <= 1'b1;
+                        
+                    end else begin
+                        
+                        Division_DONE_reg <= 1'b0;
+                    
+                    end
+                    
                                                                                     
                 end
-                DONE: begin                   
-                    Division_DONE_reg <= 1'b0;
-                
-                end           
+          
             endcase
             
         end 
          
     end
     
-
+    always_ff @(posedge clk) begin
+    
+        if(counter == 32'd33) begin
+            $display("Quotient: %0d",quotient);
+            $display("Remainder: %0d",remainder);
+            $display("Counter: %0d",counter);
+            $display("Done Signal: %0d", Division_DONE);
+            $display("Current State: %s", current_state);
+            $display("next State: %s", next_state);
+            $display("Division Start flag: %d", Division_START);
+            $display("\n");
+        end
+        
+       
+    
+    
+    
+    end
+    
+    
     assign remainder = remainder_reg;
+    assign quotient = quotient_reg;
     assign Division_DONE = Division_DONE_reg;
    
 
--- rtl/cpu/core/execute.sv
+++ rtl/cpu/core/execute.sv
@@ -20,8 +20,8 @@ import rv32_pkg::*;
  input  logic                           rst, // Active-low asynchronous reset
  input  logic                           ctrl_i, // Control signals to execute
  input  logic  [DATA_WIDTH-1:0]         op_a_i, // Register A operand (data) from RF
- input  logic  [DATA_WIDTH-1:0]         op_b_i, // Register B operand (data) from RF
- input  logic  [3:0]                    ALU_select, // Is driven by decoder Funct 3 & 7 bundled together
+ input  logic  [DATA_WIDTH-1:0]         op_b_i, // Register B operand (data) or sign extended immediate 32'b values
+ input  logic  [4:0]                    ALU_OP,
  output logic  [DATA_WIDTH-1:0]         alu_res_o, // ALU result from procesing operands 
  output logic  [DATA_WIDTH-1:0]         ALU_OUTPUT,
  output logic                           branch_taken_o, // Control signal for whether branch should be taken
@@ -43,12 +43,22 @@ import rv32_pkg::*;
     
     always_comb begin // ALU selection OPCODE
         ALU_OUTPUT = 32'b0; // Defaults
+
+        case(ALU_OP)
         
-        case(ALU_select)
-            FUNCT3_ADD: ALU_OUTPUT = op_a_i + op_b_i; // I need to know if to sign extend or not to detect unsigned/signed overflow
+            FUNCT3_ADD: ALU_OUTPUT = op_a_i + op_b_i; 
             FUNCT3_SUB: ALU_OUTPUT = op_a_i - op_b_i; 
-       
-       
+            FUNCT3_SLL: ALU_OUTPUT = op_a_i << op_b_i;
+            FUNCT3_SLT: ALU_OUTPUT = ($signed(op_a_i) < $signed(op_b_i)) ? 32'd1 : 32'd0; // if A < b return 1 else 0 SIGNED
+            FUNCT3_SLTU: ALU_OUTPUT = (op_a_i < op_b_i) ? 32'd1 : 32'd0; // if A < b return 1 else 0 UNSIGNED
+            FUNCT3_XOR: ALU_OUTPUT = op_a_i ^ op_b_i; // will XOR every individual bit
+            FUNCT3_SRL: ALU_OUTPUT = op_a_i >> op_b_i;
+            FUNCT3_OR: ALU_OUTPUT = op_a_i | op_b_i;
+            FUNCT3_AND: ALU_OUTPUT = op_a_i & op_b_i;
+            FUNCT7_SRA: ALU_OUTPUT = op_a_i >>> op_b_i;
+            // add mul signed and unsigned & signed unsigned divider
+            
+            
        endcase
             
     
