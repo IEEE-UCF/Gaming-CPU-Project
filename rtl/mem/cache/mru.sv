aff6bc7956f68b363003ba3de1fa946b2e8696a3
anthonysokolov76 <an711886@ucf.edu>
2026-02-20 18:56:36 -0500

    MRU replacement policy v2

--- rtl/mem/cache/mru.sv
+++ rtl/mem/cache/mru.sv
@@ -1,4 +1,4 @@
-// Most Recently Used (Assuming two-way cache)
+// most recently used replacement policy (Updated for N-way cache)
 
 module mru #(
     parameter int SETS = 128,
@@ -8,53 +8,58 @@ module mru #(
     input logic rst_ni,
 
     input logic [$clog2(SETS)-1:0] set_index_i,
-    input logic isWay0_valid_i,
-    input logic isWay1_valid_i,
+    input logic [WAYS-1:0] isWay_valid_i,
+
+    input  logic [$clog2(WAYS)-1:0] way_hit_i,
+    input  logic [$clog2(WAYS)-1:0] way_filled_i,
 
     input logic isthere_hit_i,
     input logic isthere_miss_i,
     input logic isfilled_i,
-    input logic way_filled_i,
-    input logic way_hit_i,
 
-    output logic way_to_evict_o
-
+    output logic [$clog2(WAYS)-1:0] way_to_evict_o
 );
 
-  logic mru_bits[SETS];
+  logic [$clog2(WAYS)-1:0] mru_way[SETS]; // stores index of MRU way
 
   always_comb begin
-    way_to_evict_o = 1'b0;  // way 0 to be evicted by default
+    way_to_evict_o = '0;
+    logic invalid_found = 1'b0;
 
-    // if there's a cache miss, a way will need to be used (evicted)
     if (isthere_miss_i) begin
+      for (int i = 0; i < WAYS; i++) begin
+        if (!isWay_valid_i[i] && !invalid_found) begin
+          way_to_evict_o = i; // look for invalid way to evict first
+          invalid_found = 1'b1;
+        end
+      end
 
-      if (!isWay0_valid_i) way_to_evict_o = 1'b0;  // if way 0 is empty, use it
-      else if (!isWay1_valid_i)
-        way_to_evict_o = 1'b1;  // if way 0 isn't empty, but way 1 is, use way 1
-      else
-        // both ways aren't empty, evict way marked by inverted flag
-        way_to_evict_o = ~mru_bits[set_index_i];
+      if (!invalid_found) begin // if no invalid, evict first way that isn't MRU
+        for (int i = 0; i < WAYS; i++) begin
+          if (i != mru_way[set_index_i]) begin
+            way_to_evict_o = i;
+            break;
+          end
+        end
+      end
 
     end
   end
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
 
-    if (!rst_ni) begin
-      foreach (mru_bits[i]) mru_bits[i] <= 1'b0;
-
-    end else if (isthere_hit_i) begin
-
-      // if there's a hit, MRU bit is updated to the way that was hit
-      mru_bits[set_index_i] <= way_hit_i;
-
-    end else if (isfilled_i) begin
-
-      // if a way is filled, use that way as the victim way
-      mru_bits[set_index_i] <= way_filled_i;
-
+    if (!rst_ni) begin // reset logic
+      for (int i = 0; i < SETS; i++) begin
+        mru_way[i] <= '0;
+      end
+    end else begin
+      if (isthere_hit_i) begin
+        mru_way[set_index_i] <= way_hit_i; // if there's a hit, way was used -- MRU
+      end else if (isfilled_i) begin
+        mru_way[set_index_i] <= way_filled_i; // if a way is filled, way will soon be used -- MRU
+      end
     end
+
   end
 
 endmodule
