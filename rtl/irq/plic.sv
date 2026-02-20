7f752a5fb84ec6ddaf9e4337bcc012aeb6aca58f
Gavin Wiese <gavinwiese1220@gmail.com>
2026-02-20 14:51:04 -0500

    Update plic.sv

--- rtl/irq/plic.sv
+++ rtl/irq/plic.sv
@@ -1,17 +1,20 @@
-module plic #(
-    parameter NSOURCES     = 32,
-    parameter PRIO_WIDTH   = 3,
-    parameter SRC_ID_WIDTH = 5
-)(
-    input  logic clk_i,
-    input  logic rst_ni,
-    input  logic [NSOURCES-1:0] src_i,       // External interrupt sources
+`timescale 1ns / 1ps
 
-    input  logic [NSOURCES*PRIO_WIDTH-1:0] priority_wdata,
-    input  logic priority_we,
-    input  logic [NSOURCES-1:0] enable_wdata,
-    input  logic enable_we,
-    input  logic claim_we,
+module PLIC #(
+    parameter NSOURCES     = 8,
+    parameter PRIO_WIDTH   = 3,
+    parameter SRC_ID_WIDTH = 3
+)(
+    input logic clk_i,
+    input logic rst_ni,
+    input logic claim_req_i,
+    input logic complete_i,
+    input logic [NSOURCES-1:0] src_i,       // External interrupt sources
+
+    input logic [NSOURCES*PRIO_WIDTH-1:0] priority_wdata,
+    input logic priority_we,
+    input logic [NSOURCES-1:0] enable_wdata,
+    input logic enable_we,
 
     output logic ext_irq_o,
     output logic [SRC_ID_WIDTH-1:0] claim_o
@@ -28,8 +31,8 @@ module plic #(
     logic [PRIO_WIDTH-1:0] tempHighestValue;
     logic activeClaim;
 
-    assign claim_o   = claim;
-    assign ext_irq_o = (tempHighestValue != 0);
+    assign claim_o   = activeClaim ? (claim + 1'b1) : '0;
+    assign ext_irq_o = (!activeClaim) && (tempHighestValue != 0);
 
     // -------------------------
     // Priority selector
@@ -39,8 +42,7 @@ module plic #(
         highestPriorIndex = 0;
 
         for (int i = 0; i < NSOURCES; i++) begin
-            if (pending[i] &&
-                priorities[i*PRIO_WIDTH +: PRIO_WIDTH] > tempHighestValue) begin
+            if (pending[i] && enable[i] && priorities[i*PRIO_WIDTH +: PRIO_WIDTH] > tempHighestValue) begin
                 tempHighestValue  = priorities[i*PRIO_WIDTH +: PRIO_WIDTH];
                 highestPriorIndex = i[SRC_ID_WIDTH-1:0];
             end
@@ -66,16 +68,21 @@ module plic #(
             if (enable_we)
                 enable <= enable_wdata;
 
-            if (claim_we)
-                activeClaim <= 0;   // Claim complete
-
             // Pending interrupt latching
             for (int i = 0; i < NSOURCES; i++) begin
-                pending[i] <= pending[i] | (src_i[i] & enable[i]);
+                pending[i] <= pending[i];
+                if(!(activeClaim && (i == claim))) begin
+                    pending[i] <= pending[i] | src_i[i];
+                end
             end
 
+            //Complete logic
+            if (complete_i && activeClaim) begin
+                activeClaim <= 0;
+            end
+            
             // Claim logic
-            if (!activeClaim && tempHighestValue != 0) begin
+            if (claim_req_i && !activeClaim && tempHighestValue != 0) begin
                 claim <= highestPriorIndex;
                 pending[highestPriorIndex] <= 0;
                 activeClaim <= 1;
@@ -83,3 +90,4 @@ module plic #(
         end
     end
 endmodule
+
