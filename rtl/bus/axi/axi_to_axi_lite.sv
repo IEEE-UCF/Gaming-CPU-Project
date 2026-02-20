a59f7cec4aadb7cf04801fe1d9ba517df7a62cc7
ConversePrawn <astauffer1116@gmail.com>
2026-02-20 18:45:42 -0500

    Update axi_to_axi_lite.sv

--- rtl/bus/axi/axi_to_axi_lite.sv
+++ rtl/bus/axi/axi_to_axi_lite.sv
@@ -1,14 +1,54 @@
-module axi_to_axi_lite #(
-  // TODO: Parameter setups
-  parameter
-)(
-  // TODO: Port set up
-  ports
+/*
+// 
+// Written by: Arnold Stauffer
+// Purpose:
+// Receive either a read or write request from a Master to reduce the complexity of 
+// bursts of instructions. This will provide an interface for the Master to reduce workload
+// and handle register access for multiple transactions, etc.
+// 
+*/
 
-  // TODO: Input logic
-  input logic clk_i,
-  input logic rst_ni
+module axi_to_axi_lite import interconnect_pkg::*(
+    input logic clk_i,
+    input logic rst_ni,
+    
+    // Write Command Block
+    // Write Address (WR)
+    input logic [ADDR_WIDTH-1:0] awaddr_i,
+    input logic awvalid_i,
+    output logic awready_o,
+    // Write (W)
+    input logic [DATA_WIDTH-1:0] wdata_i,
+    input logic [(DATA_WIDTH/8)-1:0] wstrb,  // Bytes valid to write
+    input logic wvalid_i,
+    output logic wready_o,
+    // Write Response (B)
+    output logic bready_o,
+    input logic bready_i,
   
+    // Read Command Block
+    // Read Address (R)
+    input logic [ADDR_WIDTH-1:0] araddr_i,
+    input logic arvalid_i,
+    output logic arready_o,
+    // Read Response (R)
+    output logic [DATA_WIDTH-1:0] rdata_o,
+    output logic rvalid_o,
+    input logic rready_i
 );
 
+    // Captures Writing Requests
+    logic [ADDR_WIDTH-1:0] awaddr_reg;
+    logic [DATA_WIDTH-1:0] wdata_reg;
+    logic [[(DATA_WIDTH/8)-1:0] wstrb_reg;
+  
+    // Captures Reading Requests
+    logic [ADDR_WIDTH-1:0] raddr_reg;
+  
+    function automatic int unsigned indexing(input logic [ADDR_WIDTH-1:0] addr);
+      indexing = addr[ADDR_WIDTH-1:2];
+    endfunction
+
+    
 endmodule
+
