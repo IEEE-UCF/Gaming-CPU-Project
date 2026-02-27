1e46f75d0fbf7f1dbd7ee98f4fcbd9bb09c2f7ed
Nidal Rahman <nidal.rahman@ucf.edu>
2026-02-27 11:57:00 -0500

    Update axi_icache_port.sv

--- rtl/bus/axi/axi_icache_port.sv
+++ rtl/bus/axi/axi_icache_port.sv
@@ -8,7 +8,6 @@ module axi_icache_port #(
   parameter int unsigned AXI_USER_WIDTH = 1,
 
 
-
 )(
   // TODO: Port set up ports
 
@@ -16,28 +15,32 @@ module axi_icache_port #(
   input logic clk_i,
   input logic rst_ni
 
-  // icache valid request and address
-  input logic                         ic_req_valid_i,
-  input logic[AXI_ADDR_WIDTH-1:0]     ic_addr_valid_i,
 
-  // icache
+  // icache valid request and address
+  input logic                             ic_req_valid_i,
+  input logic[AXI_ADDR_WIDTH-1:0]         ic_addr_valid_i,
+  input logic
+
+  // icache 
 
 
   // axi address read signals
-  output logic axi_mem_ar_o,
+  output logic [AXI_ADDR_WIDTH-1:0]       axi_mem_ar_o,
   output logic axi_ar_valid_o, 
   input logic axi_ar_ready_i, 
+  
+
 
   // axi read data signals
   input logic axi_mem_r_i,
   input logic axi_r_ready_i, 
   input logic axi_r_valid_i,
-
+  input logic [AXI_ADDR_WIDTH*4-1:0]      axi_mem_r_i,
 
   typedef enum [2:0] {
-    IDLE;					  // Do nothing, wait for i$ to miss
-    AR_SEND;				// cache requested a line, put it on araddr and set ARVALID to high
-    R_COLLECT;      // collect the requested data from the crossbar and send it to cache
+    IDLE,					  // Do nothing, wait for icache to miss
+    AR_SEND,				// cache requested a line, put it on araddr and set ARVALID to high
+    R_COLLECT,      // collect the requested data from the crossbar and send it to cache, once rlast is recieved, flip back to idle
 
   } icache_port_state_e;
 
@@ -52,7 +55,29 @@ module axi_icache_port #(
       current_state <= next_state;
     end
   end
-  
+
+
+  // state transition logic
+  always_comb begin 
+    next_state = current_state;
+    case (current_state)
+      IDLE: begin
+        if (conditions) next_state = AR_SEND;
+
+      end
+
+      AR_SEND: begin
+        if (conditions) next_state = R_COLLECT;
+
+      end
+
+      R_COLLECT: begin
+        if (conditions) next_state = IDLE;
+
+      end
+    endcase
+    
+  end
 );
 
 endmodule
