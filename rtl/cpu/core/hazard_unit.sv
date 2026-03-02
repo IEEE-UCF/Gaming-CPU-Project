f09c0cd745c02815e35e739421052866ecda5011
MatiasYezzi <matias@ybhome.net>
2026-03-01 20:23:04 -0500

    Updates/Changes to decode.sv, hazard_unit.sv, and rv32_pkg.sv

--- rtl/cpu/core/hazard_unit.sv
+++ rtl/cpu/core/hazard_unit.sv
@@ -9,11 +9,11 @@ module hazard_unit (
 
     // EX/MM
     input logic [4:0] ex_mm_rd_i,
-    input rv32_ctrl_s ex_mm_ctrl_i, // .reg_ex_we
+    input reg_ex_we,
 
     // MM/WB
     input logic [4:0] mm_wb_rd_i,
-    input rv32_ctrl_s mm_wb_ctrl_i, // .reg_mm_we
+    input reg_mm_we,
 
     // Forwarding controls (EX stage muxes)
     // ID/EX = 00, EX/MM = 10, MM/WB = 01
@@ -24,16 +24,16 @@ module hazard_unit (
 
 always_comb begin 
     // rs1:
-    if (ex_mm_ctrl_i.reg_ex_we && (ex_mm_rd_i == id_ex_rs1_i)) begin // Check with EX/MM rd
+    if (reg_ex_we && (ex_mm_rd_i == id_ex_rs1_i)) begin // Check with EX/MM rd
         forward_a_o = 2'b10;
-    end else if (mm_wb_ctrl_i.reg_mm_we && (mm_wb_rd_i == id_ex_rs1_i)) begin  // Check with MM/WB rd
+    end else if (reg_mm_we && (mm_wb_rd_i == id_ex_rs1_i)) begin  // Check with MM/WB rd
             forward_a_o = 2'b01;
         end
 
     // rs2:
-    if (ex_mm_ctrl_i.reg_ex_we && (ex_mm_rd_i == id_ex_rs2_i)) begin // Check with EX/MM rd
+    if (reg_ex_we && (ex_mm_rd_i == id_ex_rs2_i)) begin // Check with EX/MM rd
         forward_b_o = 2'b10;
-    end else if (mm_wb_ctrl_i.reg_mm_we && (mm_wb_rd_i == id_ex_rs2_i)) begin  // Check with MM/WB rd
+    end else if (reg_mm_we && (mm_wb_rd_i == id_ex_rs2_i)) begin  // Check with MM/WB rd
             forward_b_o = 2'b01; 
         end
 end
